c00501ca4ac99cd58bf917ec4539d29ba209f449
junekinger <junekinger@hotmail.com>
2019-08-17 18:37:46 -0500

    Done!!!

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -19,13 +19,7 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        // ListController creates the hashmap object columnChoices which has the above choices
-        // Uses the ListController hashmap columnChoices to send ("All", "position type", etc)
-        // to the view via search.html Spring associates data sent through name "columns" in model.addAttribute
-        // "columns" here = data ${columns} in search.html
-        // After form input there, th:action="@{/search/results} sends control below to
-        // @RequestMapping(value = "results") with searchType ("all" , "position type", etc.)
-        // and searchTerm inputs.
+        // send data to view via model.addAttribute
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
@@ -33,18 +27,14 @@ public class SearchController {
     // TODO #1 - Create handler to process search request and display results
     @RequestMapping(value = "results")
     public String processSearchForm(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        // data that is parsed by JobData.java is an ArrayList and jobs uses a Hashmap - gives a conversion error.
-        // So to make it compatible with the ArrayList<HashMap<String, String>> below, first construct an empty
-        // ArrayList<Hashmap<String, String>>
+        // The ArrayList in JobData.java is incompatible with jobs Hashmap - so first construct an empty one
         ArrayList<HashMap<String, String>> jobs;
 
-        // if "all", JobData.javafindByValue(searchTerm) method is executed with search term and returns the
-        // hashmap "jobs" with data from there
+        // if "all", JobData.javafindByValue(searchTerm) method is executed
         if (searchType.equals("all")) {
             jobs = JobData.findByValue(searchTerm);
         } else {
-            // if not "all", JobData.findAll(searchType) method is executed and returns the ArrayList
-            // items with data
+            // if not "all", JobData.findAll(searchType) method is executed
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
         model.addAttribute("columns", ListController.columnChoices);
